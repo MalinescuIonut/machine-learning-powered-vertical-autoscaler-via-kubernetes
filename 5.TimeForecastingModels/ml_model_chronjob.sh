a023e9d4e014e3bfd38c935cf6309bbf455f34ec
Ionut Malinescu <127495026+MalinescuIonut@users.noreply.github.com>
2024-10-30 00:00:51 +0200

    Update ml_model_chronjob.sh

--- 5.TimeForecastingModels/ml_model_chronjob.sh
+++ 5.TimeForecastingModels/ml_model_chronjob.sh
@@ -1,3 +1,10 @@
-echo "*20,50 * * * * cd /python-scripts && /usr/local/bin/python arima_model.py >> forecast.log 2>&1" > /etc/cron.d/arima-forecast &&
-crontab /etc/cron.d/arima-forecast &&
+#!/bin/sh
+
+# Write the cron job to a file
+echo "*20,50 * * * * cd /python-scripts && /usr/local/bin/python arima_model.py >> forecast.log 2>&1" > /etc/cron.d/arima-forecast
+
+# Load the cron job
+crontab /etc/cron.d/arima-forecast
+
+# Run cron in the foreground
 cron -f
